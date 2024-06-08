ee2971a5629fb1b4f9d520f9280779f46a12464c
Diogolins <diogolinsx2099@gmail.com>
2024-06-07 21:59:08 -0300

    Projeto ContaBanco Finalizado

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,6 +1,45 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+import java.util.Scanner;  
+import java.util.Locale;
 
-        System.out.println("Hello, World!");
-    }
+public class ContaTerminal {
+   
+    public static void main(String[] args) throws Exception {
+    //TODO: conhecer importar a classe Scanner
+     
+        //criando Scanner
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+     
+
+     //Exibir as mensagens para o nosso usuário
+
+     //Obter pelo scanner os valores digitados no terminal
+
+     //Exibir a mensagem conta criada 
+
+    /*Numero da conta(No exemplo dado fiquei em duvida então assimilei 
+     como esse o numero da conta, já que a agência que seria como Int)
+     */
+  
+      System.out.println("Agora, digite o seu Primeiro Nome");
+      String nomeCliente = scanner.next();
+
+      System.out.println("Por favor, digite o numero da sua Agência " );
+      int agencia = scanner.nextInt(); 
+    
+      System.out.println("Agora, digite o numero da sua Conta");
+      String numero = scanner.next();
+
+      System.out.println("Agora, digite o seu Saldo");
+      Double Saldo = scanner.nextDouble();
+
+   
+
+    //Resolução para o cliente
+
+      System.out.println("Olá" + " "  + nomeCliente + " " + "obrigado por criar uma conta em nosso banco a sua agência é" + " "
+      + agencia + " "+ ", conta" + " " + numero + " " + "e seu saldo de " + " " +
+      + Saldo + "R$" + " " + "já está disponível para saque");
+
+ }
 }
