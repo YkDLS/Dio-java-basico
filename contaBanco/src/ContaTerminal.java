a7a019cc3851a3f69b40fc961af60e8c5bfb0a32
Diogolins <diogolinsx2099@gmail.com>
2024-06-07 22:10:39 -0300

    Projeto conta banco Finalizado

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class ContaTerminal {
-   
+ 
     public static void main(String[] args) throws Exception {
     //TODO: conhecer importar a classe Scanner
-     
-        //criando Scanner
-     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-     
+
 
      //Exibir as mensagens para o nosso usuário
 
@@ -17,29 +13,33 @@ public class ContaTerminal {
 
      //Exibir a mensagem conta criada 
 
-    /*Numero da conta(No exemplo dado fiquei em duvida então assimilei 
+     /*Numero da conta(No exemplo dado fiquei em duvida então assimilei 
      como esse o numero da conta, já que a agência que seria como Int)
      */
-  
-      System.out.println("Agora, digite o seu Primeiro Nome");
-      String nomeCliente = scanner.next();
+     
+        try (//criando Scanner
+    Scanner novoScanner = new Scanner(System.in).useLocale(Locale.US)) {
 
-      System.out.println("Por favor, digite o numero da sua Agência " );
-      int agencia = scanner.nextInt(); 
-    
-      System.out.println("Agora, digite o numero da sua Conta");
-      String numero = scanner.next();
+        System.out.println("Agora, digite o seu Primeiro Nome");
+          String nomeCliente = novoScanner.next();
 
-      System.out.println("Agora, digite o seu Saldo");
-      Double Saldo = scanner.nextDouble();
+          System.out.println("Por favor, digite o numero da sua Agência " );
+          int agencia = novoScanner.nextInt(); 
+        
+          System.out.println("Agora, digite o numero da sua Conta");
+          String numero = novoScanner.next();
+
+          System.out.println("Agora, digite o seu Saldo");
+          Double Saldo = novoScanner.nextDouble();
 
    
 
-    //Resolução para o cliente
+        //Resolução para o cliente
 
-      System.out.println("Olá" + " "  + nomeCliente + " " + "obrigado por criar uma conta em nosso banco a sua agência é" + " "
-      + agencia + " "+ ", conta" + " " + numero + " " + "e seu saldo de " + " " +
-      + Saldo + "R$" + " " + "já está disponível para saque");
-
+          System.out.println("Olá" + " "  + nomeCliente + " " + "obrigado por criar uma conta em nosso banco a sua agência é" + " "
+          + agencia + " "+ ", conta" + " " + numero + " " + "e seu saldo de " + " " +
+          + Saldo + "R$" + " " + "já está disponível para saque");
+    }
+        
  }
 }
